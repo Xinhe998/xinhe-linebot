eafb99a69302ad374fa656230a82d97a79245031
Xinhe <xinhe998@gmail.com>
2019-04-29 19:56:33 +0800

    update

--- app.js
+++ app.js
@@ -34,14 +34,35 @@ bot.on('message', (event) => {
   let replyMsg;
   event.source.profile().then((profile) => {
     replyMsg = `Hello, ${profile.displayName}`;
-    event.reply(replyMsg);
   });
-
-  // event.reply(replyMsg).then((data) => {
-  //   console.log(data);
-  // }).catch((error) => {
-  //   console.log(error);
-  // });
+  event.reply({
+    type: 'template',
+    altText: 'this is a buttons template',
+    template: {
+      type: 'buttons',
+      thumbnailImageUrl: 'https://example.com/bot/images/image.jpg',
+      title: 'Menu',
+      text: 'Please select',
+      actions: [{
+        type: 'postback',
+        label: 'Buy',
+        data: 'action=buy&itemid=123',
+      }, {
+        type: 'postback',
+        label: 'Add to cart',
+        data: 'action=add&itemid=123',
+      }, {
+        type: 'uri',
+        label: 'View detail',
+        uri: 'http://example.com/page/123',
+      }],
+    },
+  });
+  event.reply(replyMsg).then((data) => {
+    console.log(data);
+  }).catch((error) => {
+    console.log(error);
+  });
 });
 
 bot.on('follow', (event) => {
