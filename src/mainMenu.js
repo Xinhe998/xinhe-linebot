65b403d552f2cde5e4e3a434eadcaf42d31f23cd
Xinhe <xinhe998@gmail.com>
2019-05-02 17:50:13 +0800

    complete 🎉

--- src/mainMenu.js
+++ src/mainMenu.js
@@ -4,39 +4,96 @@ export default (event) => {
   client.getProfile(event.source.userId)
     .then((profile) => {
       client.replyMessage(event.replyToken, {
-        type: 'template',
+        type: 'flex',
         altText: 'Hi, I am Xinhe.',
-        template: {
-          type: 'buttons',
-          thumbnailImageUrl: 'https://i.imgur.com/gopa6mjl.png',
-          title: `${profile.displayName}, 很高興認識你！`,
-          text: '想從哪方面開始聊呢？',
-          actions: [
-            {
-              type: 'postback',
-              label: 'Xinhe自我介紹一下吧！',
-              text: '自我介紹',
-              data: '自我介紹',
-            },
-            {
-              type: 'postback',
-              label: '實習工作經歷',
-              data: '實習工作經歷',
-              text: '實習工作經歷',
-            },
-            {
-              type: 'postback',
-              label: '專案作品',
-              data: '專案作品',
-              text: '專案作品',
-            },
-            {
-              type: 'postback',
-              label: '專長＆技能樹',
-              data: '專長＆技能樹',
-              text: '專長＆技能樹',
-            },
-          ],
+        contents: {
+          type: 'bubble',
+          direction: 'ltr',
+          hero: {
+            type: 'image',
+            url: 'https://i.imgur.com/gopa6mjl.png',
+            size: 'full',
+            aspectRatio: '1.51:1',
+            aspectMode: 'fit',
+          },
+          body: {
+            type: 'box',
+            layout: 'vertical',
+            contents: [
+              {
+                type: 'text',
+                text: `${profile.displayName}, 很高興認識你！`,
+                align: 'start',
+                weight: 'bold',
+              },
+              {
+                type: 'text',
+                text: '想從哪方面開始聊呢？',
+                align: 'start',
+                margin: 'md',
+                size: 'md',
+                color: '#787878',
+              },
+            ],
+          },
+          footer: {
+            type: 'box',
+            layout: 'vertical',
+            contents: [
+              {
+                type: 'button',
+                action: {
+                  type: 'postback',
+                  label: 'Xinhe自我介紹一下吧！',
+                  text: '自我介紹',
+                  data: '自我介紹',
+                },
+                color: '#72647A',
+                style: 'link',
+                margin: 'none',
+                height: 'sm',
+              },
+              {
+                type: 'button',
+                action: {
+                  type: 'postback',
+                  label: '實習工作經歷',
+                  text: '實習工作經歷',
+                  data: '實習工作經歷',
+                },
+                color: '#72647A',
+                style: 'link',
+                margin: 'none',
+                height: 'sm',
+              },
+              {
+                type: 'button',
+                action: {
+                  type: 'postback',
+                  label: '專案作品',
+                  text: '專案作品',
+                  data: '專案作品',
+                },
+                color: '#72647A',
+                style: 'link',
+                margin: 'none',
+                height: 'sm',
+              },
+              {
+                type: 'button',
+                action: {
+                  type: 'postback',
+                  label: '專長＆技能樹',
+                  text: '專長＆技能樹',
+                  data: '專長＆技能樹',
+                },
+                color: '#72647A',
+                style: 'link',
+                margin: 'none',
+                height: 'sm',
+              },
+            ],
+          },
         },
       });
     });
